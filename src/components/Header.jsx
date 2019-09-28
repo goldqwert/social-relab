2f74bf65b0befbe0c5ac90e676f0a535868d92c1
Yury <goldyukol12@gmail.com>
2019-09-28 13:34:39 +0300

    add: add files to work with the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://s2.logaster.com/static/v3/img/products/logo.png" alt='text'></img>
+    </header>
+}
+
+export default Header;
