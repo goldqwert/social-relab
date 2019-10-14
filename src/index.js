26902abf5ce91d35b878ad61c3cdd920f87c78c0
Yury <goldyukol12@gmail.com>
2019-10-14 15:37:55 +0300

    feat: create render.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import state from './redux/state'
-import { addPost } from './redux/state'
+import { rerenderEntireTree } from './render';
 
 
-ReactDOM.render(<App state={state} addPost={addPost} />, document.getElementById('root'));
+rerenderEntireTree(state);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from "../render";
+
 let state = {
     profilePage: {
         posts: [
@@ -34,13 +36,14 @@ let state = {
 }
 
 export let addPost = (postMessage) => {
-    debugger;
     let newPost = {
         id: 5,
         message: postMessage,
         likesCount: 0
+
     }
-    state.profilePage.posts.push(newPost)
+    state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 export default state;
