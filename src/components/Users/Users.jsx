cca073741b59bb7c50c6076b19c02cd9d8bf0a06
Yury <goldyukol12@gmail.com>
2019-11-21 14:00:25 +0300

    feat: add componentDidMount in users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,9 +5,8 @@ import photoNull from '../../assets/img/photoNull.png'
 
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props)
 
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
