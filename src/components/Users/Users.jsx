36c10035276bda0391d0afbba3b523f49e2a813a
Yury <goldyukol12@gmail.com>
2019-11-29 22:38:00 +0300

    feat: add requests in follow/unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import s from './Users.module.css';
 import photoNull from '../../assets/img/photoNull.png'
 import { NavLink } from 'react-router-dom'
-
+import * as axios from 'axios'
 
 let Users = (props) => {
 
@@ -31,8 +31,31 @@ let Users = (props) => {
                     </NavLink>
                 </div>
 
-                {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
-                    <button onClick={() => props.follow(u.id)}>Follow</button>}
+                {u.followed ? <button onClick={() => {
+
+                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        withCredentials: true,
+                        headers: {
+                            'API-KEY': 'e95ce95d-8f55-48e7-8249-188812878de1'
+                        }
+                    }).then(response => {
+                        if (response.data.resultCode === 0) {
+                            props.unfollow(u.id)
+                        }
+                    })
+                }}>Unfollow</button> :
+                    <button onClick={() => {
+                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                            withCredentials: true,
+                            headers: {
+                                'API-KEY': 'e95ce95d-8f55-48e7-8249-188812878de1'
+                            }
+                        }).then(response => {
+                            if (response.data.resultCode === 0) {
+                                props.follow(u.id)
+                            }
+                        })
+                    }}>Follow</button>}
 
                 <div>{u.name}</div>
                 <div>{u.status}</div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,23 +10,25 @@ class UsersContainer extends React.Component {
 
     componentDidMount() {
         this.props.preloaderIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-                this.props.preloaderIsFetching(false)
-            })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        }).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+            this.props.preloaderIsFetching(false)
+        })
 
     }
 
     onPageChanged = (pageNumber) => {
         this.props.preloaderIsFetching(true)
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.preloaderIsFetching(false)
-            })
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        }).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.preloaderIsFetching(false)
+        })
     }
 
 
